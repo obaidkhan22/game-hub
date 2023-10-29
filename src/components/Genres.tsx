cc85377bc109af3aa99c9afb34bc410ab455c31a
Obaidullah Totkhail <obaidhamdard69@gmail.com>
2023-10-28 20:36:38 -0700

    Fetching the genres.

---
+++ src/components/Genres.tsx
@@ -0,0 +1,13 @@
+import useGenres from "../hooks/useGenres";
+const GenresList = () => {
+  const { data } = useGenres();
+  return (
+    <ul>
+      {data.map((genre) => (
+        <li key={genre.id}>{genre.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default GenresList;
