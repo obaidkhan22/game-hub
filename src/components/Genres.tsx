17baf2cd6e8726a30aac4ecd1180edcb50c967d3
Obaidullah Totkhail <obaidhamdard69@gmail.com>
2023-10-28 20:55:25 -0700

    displaying genres.

--- src/components/Genres.tsx
+++ src/components/Genres.tsx
@@ -1,12 +1,23 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImages from "./../service/img-url";
 const GenresList = () => {
   const { data } = useGenres();
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY={1}>
+          <HStack>
+            <Image
+              boxSize="40px"
+              borderRadius={8}
+              src={getCroppedImages(genre.image_background)}
+            />
+            <Text fontSize="18px">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
