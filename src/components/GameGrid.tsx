8514d363494ad40aa66beed8314386411f39aa60
Obaidullah Totkhail <obaidhamdard69@gmail.com>
2023-10-31 21:47:38 -0700

    cleaning up the game card.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -17,8 +17,8 @@ export const GameGrid = ({ gameQuery }: Props) => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={5}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 3 }}
+        spacing={8}
         padding={5}
       >
         {isLoading &&
