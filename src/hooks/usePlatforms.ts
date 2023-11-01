08039e43bfebaf0ce0a40ce588826839a701b6f2
Obaidullah Totkhail <obaidhamdard69@gmail.com>
2023-10-31 21:41:13 -0700

    updated the code

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,4 @@
 import platforms from "../data/platforms";
-
-interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
-
 const usePlatforms = () => ({ data: platforms, error: null });
 
 export default usePlatforms;
